e64ad59b7f5f97d4b463c335c968ae1aba46df28
blackdragonbonu <albertssj25@gmail.com>
2018-04-20 15:34:10 -0400

    Corrected some errors and made changes to code

--- code/seq_to_seq.py
+++ code/seq_to_seq.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import pickle
 from QuestionAnswers import QuestionAnswers
 from nltk.tokenize import word_tokenize
+from tensorflow.python.layers import core as layers_core
 
 #File locations for laoding glove and pickle dump of questions and answers
 
@@ -21,11 +22,13 @@ class seq_to_seq():
         self.length_answers=[]
         self.train_questions=[]
         self.train_answers=[]
-        self.train_question_array=np.zeros((self.train_size,self.Max_time))
-        self.train_answer_array=np.zeros((self.train_size,self.Max_time))
+        self.train_question_array=np.zeros((self.train_size,self.Max_time+1),dtype=np.int32)
+        self.train_answer_array=np.zeros((self.train_size,self.Max_time),dtype=np.int32)
+        
 
     def prepare_glove_vector(self):
         self.word2idx={'PAD':0,'STOP':1,'START':2,'UNK':3}
+        self.index2word={0:'PAD',1:'STOP',2:'START',3:'UNK'}
         self.weights=[np.random.randn(300) for _ in range(4)]
         with open(self.glove_file_location) as glove_file:
             i=0
@@ -35,43 +38,48 @@ class seq_to_seq():
                 word_weights=np.asarray(line_split[1:],dtype=np.float32)
                 self.weights.append(word_weights)
                 self.word2idx[word.lower()]=index+4
+                self.index2word[index+4]=word.lower()
                 if index>48000:
                     break
         self.weights=np.asarray(self.weights,dtype=np.float32)
+
     def prepare_questions_and_answers(self):
         pickle_file_location='../data/squad/qa_dump'
         Question_answers=[]
         self.Vocab_size=len(self.word2idx)
-        self.train_answer_array_one_hot=np.zeros(shape=(self.train_size,self.Max_time,self.Vocab_size))
+        self.train_question_array_one_hot=np.zeros(shape=(self.train_size,self.Max_time,self.Vocab_size))
         with open(self.pickle_file_location) as pickle_file:
             Question_answers=pickle.load(pickle_file)[0]
         for i,qa in enumerate(Question_answers[:self.train_size]):
             tokenized_question=word_tokenize((qa.question))
-            self.length_questions.append(len(tokenized_question))
-            new_question=[]
-            for word in tokenized_question:
+            self.length_questions.append(len(tokenized_question)+1)
+            new_question=[self.word2idx['START']]
+            new_question_one_hot=np.zeros(shape=(self.Max_time,self.Vocab_size))
+            for j,word in enumerate(tokenized_question):
+                new_question_one_hot[i,self.word2idx.get(word.lower(),self.word2idx['UNK'])]=1
                 new_question.append(self.word2idx.get(word.lower(),self.word2idx['UNK']))
-            while len(new_question)<self.Max_time:
+            new_question_one_hot[j,self.word2idx['STOP']]=1
+            new_question.append(self.word2idx['STOP'])
+            while len(new_question)<self.Max_time+1:
                 new_question.append(0)
+                new_question_one_hot[j,self.word2idx['PAD']]=1
             self.train_questions.append(np.array(new_question))
-            self.train_question_array[i]=np.array(new_question[:self.Max_time])
+            self.train_question_array[i]=np.array(new_question[:self.Max_time+1])
         
             tokenized_answer=word_tokenize(qa.answer)
             self.length_answers.append(len(tokenized_answer))
             new_answer=[]
-            new_answer_one_hot=np.zeros(shape=(self.Max_time,self.Vocab_size))
             for j,word in enumerate(tokenized_answer):
-                new_answer_one_hot[i,self.word2idx.get(word.lower(),self.word2idx['UNK'])]=1
                 new_answer.append(self.word2idx.get(word.lower(),self.word2idx['UNK']))
-            new_answer_one_hot[j,self.word2idx['STOP']]=1
-            new_answer.append(self.word2idx['STOP'])
+            
             while len(new_answer)<self.Max_time:
                 j+=1
-                new_answer_one_hot[j,self.word2idx['PAD']]=1
                 new_answer.append(0)
             self.train_answers.append(np.array(new_answer))
             self.train_answer_array[i]=np.array(new_answer[:self.Max_time])
-            self.train_answer_array_one_hot[i,:,:]=np.array(new_answer_one_hot[:self.Max_time,:])
+            self.train_question_array_one_hot[i,:,:]=np.array(new_question_one_hot[:self.Max_time,:])
+            self.length_answers_array=np.array(self.length_answers)
+            self.length_questions_array=np.array(self.length_questions)
 
     def train_model(self):
         self.batch_size=25
@@ -85,10 +93,15 @@ class seq_to_seq():
         
         
         target_out=tf.placeholder(dtype=tf.float32,shape=[None,None,self.Vocab_size])
+        # target_out=tf.placeholder(dtype=tf.float32,shape=[self.batch_size,self.Max_time])
         encoder_inputs=tf.placeholder(dtype=tf.int32,shape=[self.batch_size,self.Max_time])
         decoder_inputs=tf.placeholder(dtype=tf.int32,shape=[self.batch_size,self.Max_time])
-        encoder_lengths=tf.placeholder(dtype=tf.int32,shape=None)
-        decoder_lengths=tf.placeholder(dtype=tf.int32,shape=None)
+        #target_out=tf.placeholder(dtype=tf.int32,shape=[self.Max_time,self.batch_size])
+        # encoder_inputs=tf.placeholder(dtype=tf.int32,shape=[self.Max_time,self.batch_size])
+        # decoder_inputs=tf.placeholder(dtype=tf.int32,shape=[self.Max_time,self.batch_size])
+        encoder_lengths=tf.placeholder(dtype=tf.int32,shape=self.batch_size)
+        decoder_lengths=tf.placeholder(dtype=tf.int32,shape=self.batch_size)
+        scores=layers_core.Dense(self.Vocab_size, use_bias=False)
 
         with tf.variable_scope('encoder'):
             encoder_input_embeds=tf.nn.embedding_lookup(embedding_weights,encoder_inputs)
@@ -104,40 +117,80 @@ class seq_to_seq():
                                                         initial_state=encoder_states,
                                                         sequence_length=decoder_lengths,
                                                         dtype=tf.float32)
+        # with tf.variable_scope('decoder'):
+            
+        #     decoder_inputs_embeds=tf.nn.embedding_lookup(embedding_weights,decoder_inputs)
+            
+        #     print "Decoder input shape",decoder_inputs.shape
+        #     print "Decoder Lengths : ",decoder_lengths
+        #     decoder_lstm_cell=tf.nn.rnn_cell.BasicLSTMCell(self.lstm_size)
+        #     helper = tf.contrib.seq2seq.TrainingHelper(
+        #             decoder_inputs_embeds, decoder_lengths, time_major=True)
+        #     decoder = tf.contrib.seq2seq.BasicDecoder(
+        #                     decoder_lstm_cell, helper, initial_state=encoder_states,
+        #                     output_layer=scores)
+        #     decoder_output,final_context,_=tf.contrib.seq2seq.dynamic_decode(decoder,
+        #                         output_time_major=False,swap_memory=True,
+        #                         maximum_iterations=self.Max_time)
+        #     logits=decoder_output.rnn_output
 
         weights=tf.Variable(tf.random_normal([self.lstm_size,self.Vocab_size]))
         biases=tf.Variable(tf.random_normal([self.Vocab_size]))
         sess = tf.Session()
-
+        
         scores=tf.einsum('ijk,kl -> ijl',decoder_output,weights)+biases
         #training_loss=tf.losses.mean_squared_error(target_out,scores)
-        training_loss=tf.losses.softmax_cross_entropy(target_out,scores)
+        #print logits.shape
+        #print target_out.shape
+        cross_ent=tf.nn.softmax_cross_entropy_with_logits(labels=target_out,logits=scores)
+        training_loss=(tf.reduce_sum(cross_ent*tf.cast(tf.sequence_mask(decoder_lengths,
+                                        self.Max_time),tf.float32))/self.batch_size)
         params=tf.trainable_variables()
         gradients=tf.gradients(training_loss,params)
+        clipped_gradients,_=tf.clip_by_global_norm(gradients,5)
         optimizer=tf.train.AdamOptimizer(learning_rate=1e-2)
         
         sess.run(tf.local_variables_initializer())
         sess.run(tf.global_variables_initializer())  
-        update_step=optimizer.apply_gradients(zip(gradients,params))
+        update_step=optimizer.apply_gradients(zip(clipped_gradients,params))
 
         
         sess.run(tf.global_variables_initializer())
-        epoch_number=10
+        epoch_number=20
         
         for j in range(epoch_number):
             for i in range(0,self.train_size,self.batch_size):
                 print "Epoch : {0}, Sample :{1} of {2}".format(j,i,self.train_size)
+                print self.train_answer_array.shape
+                print self.length_answers_array[:10]
                 feed_dict={
-                        encoder_inputs: self.train_question_array[:self.batch_size,:],
-                        decoder_inputs: self.train_answer_array[:self.batch_size,:],
-                        target_out: self.train_answer_array_one_hot[:self.batch_size,:,:],
-                        encoder_lengths: self.length_questions[:self.batch_size],
-                        decoder_lengths: self.length_answers[:self.batch_size]
+                        encoder_inputs: (self.train_answer_array[:self.batch_size,:]),
+                        decoder_inputs: (self.train_question_array[:self.batch_size,1:]),
+                        target_out: self.train_question_array_one_hot[:self.batch_size,:self.Max_time,:],
+                        #target_out:(self.train_question_array[:self.batch_size,:self.Max_time]).T,
+                        encoder_lengths: self.length_answers_array[:self.batch_size],
+                        decoder_lengths: self.length_questions_array[:self.batch_size]
                 }
                 loss = sess.run(training_loss, feed_dict=feed_dict)
-                print loss
+                print np.mean(loss)
                 sess.run(update_step,feed_dict=feed_dict)
+                sampl_scores=sess.run(scores,feed_dict=feed_dict)
+                self.generate_sentences(sampl_scores[:5,:,:])
 
+    
+    def generate_sentences(self,word_vector):
+        decompiled_sentences=[]
+        max_vectors=np.argmax(word_vector,axis=2)
+        for sentence in max_vectors:
+            current_sentence=[]
+            for word in sentence:
+                if self.index2word[word]!='STOP':
+                    current_sentence.append(self.index2word[word]) 
+                else:
+                    break
+            print (' '.join(current_sentence))
+            decompiled_sentences.append(current_sentence)
+        return decompiled_sentences
 
 
         
